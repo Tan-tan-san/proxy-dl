fad9e4b577f54dd1863bea7d5f478d5c88bd0c14
Tan-tan-san <Tan-tan-san@users.noreply.github.com>
2014-12-05 20:03:45 +0000

    Update proxy-dl.sh
    resolving domain and replacing with IP now works. Proxy downloads now work.

--- proxy-dl.sh
+++ proxy-dl.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-echo "What is the link of the file you want to download?: "
+echo "Enter url of the file you want to download?: "
 read URL
 DOMAIN=$(echo $URL | awk -F/ '{print $3}')
 echo "Resolving hostname...."
 TOR_IP=$(tor-resolve $DOMAIN)
-RESOLVE=$(echo $URL | sed  's|\$DOMAIN|\$TOR_IP|g')
-echo "Downloading file..."
-#&> /dev/null
-proxychains wget -v $RESOLVE 
-echo "$RESOLVE"
+RESOLVE=$(echo $URL | sed "s/$DOMAIN/$TOR_IP/g")
+echo "Downloading file from: " "$RESOLVE"
+proxychains wget -v $RESOLVE &> /dev/null
 echo "Download Finished!"
