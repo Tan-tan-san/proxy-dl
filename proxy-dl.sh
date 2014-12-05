f240a9c0133d968c057bddf0287bd7ee61460a7d
Tantan-san <Tan-tan-san@users.noreply.github.com>
2014-12-05 21:02:04 +0000

    Update proxy-dl.sh
    cleaned up variables a bit.

--- proxy-dl.sh
+++ proxy-dl.sh
@@ -1,10 +1,11 @@
+#Program requires awk/sed, tor service installed and running, and proxychains with custom config.
 #!/bin/bash
-echo "Enter url of the file you want to download?: "
+echo "What is the link of the file you want to download?: "
 read URL
-DOMAIN=$(echo $URL | awk -F/ '{print $3}')
+DOMAIN=$(echo "$URL" | awk -F/ '{print $3}')
 echo "Resolving hostname...."
-TOR_IP=$(tor-resolve $DOMAIN)
-RESOLVE=$(echo $URL | sed "s/$DOMAIN/$TOR_IP/g")
+TOR_IP=$(tor-resolve "$DOMAIN")
+RESOLVE=$(echo "$URL" | sed "s/$DOMAIN/$TOR_IP/g")
 echo "Downloading file from: " "$RESOLVE"
-proxychains wget -v $RESOLVE &> /dev/null
+proxychains wget -v "$RESOLVE" &> /dev/null
 echo "Download Finished!"
